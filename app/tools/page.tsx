718b0858e6b8c834cc120823a29de02e235b6c15
Philipp <rasanteraeder@gmail.com>
2026-02-20 15:50:32 +0100

    Fix duplicate import

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Link from 'next/link'import Link from 'next/link'
+import Link from 'next/link'
 import { Search, Star, ExternalLink } from 'lucide-react'
 import { useState } from 'react'
 
